e6085b89460a1a2c27188353feeab5f03499b0c0
gazzerhaff <gazzerhaff@users.noreply.github.com>
2014-10-16 22:15:48 +0100

    Update follows.js

--- follows.js
+++ follows.js
@@ -14,7 +14,7 @@
 
 	var defaultKey		= '7qwSSd8Rkr6HJzfrPWur0z984KKRHXPDPDR1mlwDvahJanWh', // Unique master Xively API key to be used as a default
 		defaultFeeds	= [1949090795], // Comma separated array of Xively Feed ID numbers
-		applicationName	= 'temperature logger', // Replaces Xively logo in the header
+		applicationName	= 'System monitor', // Replaces Xively logo in the header
 		dataDuration	= '7days', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
 		dataInterval	= 60, // Default interval for data to be displayed (in seconds)
 		dataColor		= '', // CSS HEX value of color to represent data (omit leading #)
