159b4a8711dec6c4cf3d3e65f012eab9203e73c8
gazzerhaff <gazzerhaff@users.noreply.github.com>
2014-10-16 21:49:05 +0100

    Update follows.js

--- follows.js
+++ follows.js
@@ -150,8 +150,7 @@
 										height: 200,
 										renderer: 'line',
 										min: parseFloat(datastream.min_value) - .25*(parseFloat(datastream.max_value) - parseFloat(datastream.min_value)),
-										//max: parseFloat(datastream.max_value) + .25*(parseFloat(datastream.max_value) - parseFloat(datastream.min_value)),
-									max: 50,
+										max: parseFloat(datastream.max_value) + .25*(parseFloat(datastream.max_value) - parseFloat(datastream.min_value)),
 										padding: {
 											top: 0.02,
 											right: 0.02,
@@ -392,7 +391,7 @@
 
 	if(applicationName != '') {
 		$('h1').html(applicationName).css('color', 'white');
-		document.title = applicationName + ' - Powered by Xively';
+		document.title = applicationName + ' - by Gary Haffenden';
 	}
 
 	if(dataColor == '') {
