ca40ce8508b9ae5135b04321433a4041984ef8dc
gazzerhaff <gazzerhaff@users.noreply.github.com>
2014-10-16 20:36:28 +0100

    Update follows.js

--- follows.js
+++ follows.js
@@ -12,11 +12,11 @@
 		hideForm		= 0;
 	*/
 
-	var defaultKey		= '', // Unique master Xively API key to be used as a default
-		defaultFeeds	= [], // Comma separated array of Xively Feed ID numbers
-		applicationName	= '', // Replaces Xively logo in the header
-		dataDuration	= '', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
-		dataInterval	= 0, // Default interval for data to be displayed (in seconds)
+	var defaultKey		= '7qwSSd8Rkr6HJzfrPWur0z984KKRHXPDPDR1mlwDvahJanWh', // Unique master Xively API key to be used as a default
+		defaultFeeds	= [1949090795], // Comma separated array of Xively Feed ID numbers
+		applicationName	= 'temperature logger', // Replaces Xively logo in the header
+		dataDuration	= '7days', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
+		dataInterval	= 60, // Default interval for data to be displayed (in seconds)
 		dataColor		= '', // CSS HEX value of color to represent data (omit leading #)
 		hideForm		= 0; // To hide input form use value of 1, otherwise set to 0
 
